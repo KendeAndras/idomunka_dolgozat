827c80940504b942a2cd1bca9bfdf83dc00dd7ca
KendeAndras <totalrecord444@gmail.com>
2023-01-18 11:52:23 +0100

    kesz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*
+  File: app.component.html
+  Author: Korcsmaros Kende Andras
+  Copyright: 2023, Korcsmaros Kende Andras
+  Group: Szoft II/N
+  Date: 2023-01-18
+  Github: https://github.com/KendeAndras/
+  Licenc: GNU GPL
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +17,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'idomunka';
+
+  processLength !: number;
+  fRatePerRev !: number;
+  torque !: number;
+  ptime !: number;
+
+  calcProcessingTime():void{
+    this.ptime = this.processLength/(this.fRatePerRev*this.torque)
+  }
+
+  constructor(){}
 }
